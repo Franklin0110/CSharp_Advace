98cb31e8827cba396e9e41d60bb057a87e9c5144
Franklin0110 <franklin51822@outlook.com>
2024-01-21 17:00:55 -0600

    Pushing delegates and modifications in Interfaces practice.

---
+++ Delegates/Delegates_Part1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        //This is how a delegate is created
+        delegate void LogDel(string text);
+
+        static void Main(string[] args)
+        {
+            //We make an instance of the other class that is being used.
+            Logging log = new Logging();
+
+            //We create two objects type LogDel.
+            LogDel LogTextToScreenDel, LogTextToFileDel;
+
+            //We insert inside of the object a function
+            LogTextToScreenDel = new LogDel(log.TextToSreen);
+
+            LogTextToFileDel = new LogDel(log.TextToFile);
+
+            //we create an function that will contain two other function as summatory
+            LogDel MultiLogDel = LogTextToFileDel + LogTextToScreenDel;
+
+            Console.WriteLine("Please write your name");
+
+            var name = Console.ReadLine();
+
+            //when we call the function, it calls the other two function added before.
+            //MultiLogDel(name);
+
+            //here we pass the function as parameter that makes the same as the line that we have before. 
+            LogText(MultiLogDel, name);
+
+            Console.ReadKey();
+        }
+
+        static void LogText(LogDel logdel, string text)
+        {
+            //this calls the function passed as a function.
+            logdel(text);
+        }
+    }
+}
---
+++ Delegates/Delegates_Part1/log.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public class Logging
+    {
+
+        public void TextToSreen(String text)
+        {
+            Console.WriteLine($"{DateTime.Now} : {text}");
+
+        }
+        public void TextToFile(string text)
+        {
+            string date = DateTime.Now.ToString("yyyy-MM-dd");
+
+            //logs in a file a log with the StreamWriter object and his functions.
+            using (StreamWriter sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, $"Logs/{date}.log"), true))
+            {
+
+                sw.WriteLine($"{DateTime.Now}: {text}");
+            }
+
+        }
+    }
+}
