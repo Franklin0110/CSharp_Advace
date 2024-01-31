32569f00e885eae5b85956a6d119f04222d0c4b3
Franklin0110 <franklin51822@outlook.com>
2024-01-30 21:05:00 -0600

    Adding Validator Methods

--- ClubMembershipApplication/FieldValidatorAPI/CommonFieldValidatorFunctions.cs
+++ ClubMembershipApplication/FieldValidatorAPI/CommonFieldValidatorFunctions.cs
@@ -9,35 +9,99 @@ namespace FieldValidatorAPI
 {
     public delegate bool RequiredValidDel(string fieldVal);
     public delegate bool StringLenghtValidDel(string fieldVal, int min, int max);
-    public delegate bool DateValiDel(string dateTime, out DateTime ValiDateTime);
+    public delegate bool DateFieldValidDel(string dateTime, out DateTime ValiDateTime);
     public delegate bool FieldPatternMatchDel(string fieldVal, string pattern);
-    public delegate bool CompareFieldValiDel(string fieldVal, string FieldValCompare);
+    public delegate bool CompareFieldValidDel(string fieldVal, string FieldValCompare);
+
+
     public class CommonFieldValidatorFunctions
     {
         private static RequiredValidDel _requiredValidDel = null;
         private static StringLenghtValidDel _stringLengthValidDel = null;
-        private static DateValiDel _dateValidDel = null;
+        private static DateFieldValidDel _dateFieldValidDel = null;
         private static FieldPatternMatchDel _FieldpatternMatchValidDel = null;
-        private static CompareFieldValiDel _compareFieldsValidDel = null;
+        private static CompareFieldValidDel _compareFieldsValidDel = null;
+
+        public static FieldPatternMatchDel PatternFieldMatchDel
+        {
+            get
+            {
+                if (_FieldpatternMatchValidDel == null) _FieldpatternMatchValidDel = new FieldPatternMatchDel(FieldPatternMatch);
+
+                return _FieldpatternMatchValidDel;
+
+
+            }
+
+        }
+        public static CompareFieldValidDel FieldCompareValidDel
+        {
+            get
+            {
+                if (_compareFieldsValidDel == null) _compareFieldsValidDel = new CompareFieldValidDel(FieldComparisonValid);
+
+                return _compareFieldsValidDel;
+
+
+            }
+
+        }
+
+        public static RequiredValidDel RequiredValidDel
+        {
+            get
+            {
+                if (_requiredValidDel == null) _requiredValidDel = new RequiredValidDel(RequiredFieldValid);
+
+                return _requiredValidDel;
+
+
+            }
+
+        }
+
+        public static StringLenghtValidDel StringLengthFieldValidDel
+        {
+            get
+            {
+                if (_stringLengthValidDel == null) _stringLengthValidDel = new StringLenghtValidDel(StringLenghtValid);
+
+                return _stringLengthValidDel;
+
+
+            }
+
+        }
+
+        public static DateFieldValidDel DateFieldValidDel
+        {
+            get
+            {
+                if (_dateFieldValidDel == null) _dateFieldValidDel = new DateFieldValidDel(DateFieldValid);
+
+                return _dateFieldValidDel;
+            }
+        }
 
 
         private static bool RequiredFieldValid(string fieldVal)
         {
-            if (!string.IsNullOrEmpty(fieldVal)) return true; 
-            
-            return false;
-
-        }
-
-        private static bool StringLenghtValid(string fieldVal, int min, int max) {
-
-            if(fieldVal.Length >= min && fieldVal.Length <=max) return true;
+            if (!string.IsNullOrEmpty(fieldVal)) return true;
 
             return false;
 
         }
 
-        private static bool DateValid(string dateTime, out DateTime ValiDateTime) 
+        private static bool StringLenghtValid(string fieldVal, int min, int max)
+        {
+
+            if (fieldVal.Length >= min && fieldVal.Length <= max) return true;
+
+            return false;
+
+        }
+
+        private static bool DateFieldValid(string dateTime, out DateTime ValiDateTime)
         {
             if (DateTime.TryParse(dateTime, out ValiDateTime)) return true;
 
@@ -45,11 +109,17 @@ namespace FieldValidatorAPI
         }
 
         private static bool FieldPatternMatch(string fieldVal, string pattern)
-        { 
+        {
             Regex regex = new Regex(pattern);
 
             if (regex.IsMatch(fieldVal)) return true;
 
+            return false;
+        }
+
+        private static bool FieldComparisonValid(string field1, string field2)
+        {
+            if (field1.Equals(field2)) return true;
             return false;
         }
 
--- ClubMembershipApplication/FieldValidatorAPI/obj/Debug/net6.0/FieldValidatorAPI.AssemblyInfo.cs
+++ ClubMembershipApplication/FieldValidatorAPI/obj/Debug/net6.0/FieldValidatorAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FieldValidatorAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ce8fd6859f2a155e586a58e82794d9192f5c91e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FieldValidatorAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FieldValidatorAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
