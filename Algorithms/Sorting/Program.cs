b1bf8cac2aba08ec63cb5239147c92c50b32a44e
Franklin0110 <franklin51822@outlook.com>
2024-02-01 19:16:31 -0600

    Modifing the algorithms and creating a merge sort

--- Algorithms/Sorting/Program.cs
+++ Algorithms/Sorting/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Security.Cryptography;
-
+using System.Linq;
 
 public class Sorting
 {
@@ -9,15 +9,22 @@ public class Sorting
     {
 
         Random random = new Random();
-        int[] collection = new int[6];
+        int[] collection = new int[30];
         collection.Initialize();
         for (int iterator = 0; iterator < collection.Length; iterator++)
         {
-            collection[iterator] = random.Next(0, 10);
+            collection[iterator] = random.Next(1, 10);
         }
+        int[] arraysorted = SortHalf(collection.Take((collection.Length + 1) / 2).ToArray());
+        collection = SortHalf(collection.Skip((collection.Length + 1) / 2).ToArray());
 
-        printArray(Sort(collection));
+        printArray(collection);
     }
+
+
+
+
+
     public static void printArray(int[] array)
     {
 
@@ -27,49 +34,37 @@ public class Sorting
         }
 
     }
+    public static int[] Sort(int[] array1, int[] array2)
+    {
+
+        int i, j, k;
+        int[] array = new int[array1.Length + array2.Length];
+        i = 0;
+        j = 0;
+        k = 0;
+
+        // while(i< )
+        return array;
 
 
-    public static int[] Sort(int[] array)
+    }
+
+    public static int[] SortHalf(int[] array)
     {
 
         int _temporalValue = 0;
         int _arrayLenght = array.Length;
         for (int iterator = 0; iterator < (_arrayLenght / 2) - 1; iterator++)
         {
-            if (array[iterator] > array[iterator + 1])
+            for (int i = 0; i < array.Length - i - 1; i++)
             {
-                _temporalValue = array[iterator];
-                array[iterator] = array[iterator + 1];
-                array[iterator + 1] = _temporalValue;
-                iterator = -1;
+                if (array[i] > array[i + 1])
+                {
+                    _temporalValue = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = _temporalValue;
+                }
             }
-
-        }
-
-        for (int iterator = _arrayLenght / 2; iterator < _arrayLenght - 1; iterator++)
-        {
-            if (array[iterator] > array[iterator + 1])
-            {
-                _temporalValue = array[iterator];
-                array[iterator] = array[iterator + 1];
-                array[iterator + 1] = _temporalValue;
-                iterator = (_arrayLenght / 2) - 1;
-
-            }
-
-        }
-        //BUGGED the numbers from first half are not being ordered with the second half.
-        for (int iterator = 0; iterator < _arrayLenght / 2; iterator++)
-        {
-            if (array[iterator] > array[iterator + _arrayLenght / 2])
-            {
-                _temporalValue = array[iterator];
-                array[iterator] = array[iterator + (_arrayLenght / 2)];
-                array[iterator + (_arrayLenght / 2)] = _temporalValue;
-                iterator = 0;
-
-            }
-
         }
 
         return array;
