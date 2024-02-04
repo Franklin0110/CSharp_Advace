0577778e8ac5ea116a643cf7c627ed86d529d94a
Franklin0110 <franklin51822@outlook.com>
2024-02-04 15:29:23 -0600

    Adding the reflection_practice

---
+++ Reflection_practice/Reflection_practice/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Reflection;
+public class MainClass
+{
+
+
+    public static void Main()
+    {
+        anobject patito = new anobject();
+
+        var objecto = typeof(anobject).Assembly.GetType();
+
+        var propeties = objecto.GetMethods();
+        foreach (var prop in propeties)
+        {
+
+            var parameters = prop.GetParameters();
+            foreach (var item in parameters)
+            {
+                Console.WriteLine($"Parameter: {item.Name} : {item.ParameterType}");
+            }
+
+        }
+        Console.WriteLine(objecto);
+
+
+        Console.ReadKey();
+    }
+
+
+}
+
+public class anobject
+{
+
+    public anobject()
+    {
+        ID = 0;
+        name = "pedro";
+        somethingelse = "something";
+    }
+
+    public int ID { get; set; }
+    public string name { get; set; }
+    public string somethingelse { get; set; }
+
+}
